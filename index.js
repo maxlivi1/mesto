d78d46d622794f9fd23c9f9c7b9d488e1644a36d
Max Limanskii <maxlivi@yandex.ru>
2023-02-09 11:53:14 +0300

    feat: добавить загрузку карточек мест

--- index.js
+++ index.js
@@ -34,3 +34,45 @@ buttonFormOpen.addEventListener('click', openFormEdit);
 buttonFormClose.addEventListener('click', closeFormEdit);
 editForm.addEventListener('submit', saveFormEdit);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const placeCardTemplate = document.querySelector('#place-card').content;
+const placesCards = document.querySelector('.places');
+
+initialCards.forEach((item) => {
+  // клонируем содержимое тега template
+  const placeCardElement = placeCardTemplate.querySelector('.place').cloneNode(true);
+
+  // наполняем содержимым
+  placeCardElement.querySelector('.place__photo').src = item.link;
+  placeCardElement.querySelector('.place__photo').alt = 'Фото ' + item.name;
+  placeCardElement.querySelector('.place__name').textContent = item.name;
+
+  // отображаем на странице
+  placesCards.append(placeCardElement);
+});
